12948c9a697a96a08973a2580f029ffec204915f
Camilo Lozoya <camilo.lozoya@itesm.mx>
2021-05-07 17:38:41 -0600

    Agregar adios

--- hello.py
+++ hello.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def hello():
     return 'Hola Mundo!'
 
+@app.route("/adios/")
+def hello():
+    return 'Adios Mundo!'
+
 if __name__ == "__main__":
    #Run web server
    app.run(host='192.4.68.5', port=8000, debug=True, threaded=True) 
